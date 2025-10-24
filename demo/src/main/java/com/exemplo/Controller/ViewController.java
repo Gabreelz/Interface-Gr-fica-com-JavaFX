422a93ad4934deb8ff45b0c7e04538c44793f2af
Gabreelz <gabriel_teixeira30@hotmail.com>
2025-10-24 17:26:07 -0300

    Criando uma calculadora Simples

--- demo/src/main/java/com/exemplo/Controller/ViewController.java
+++ demo/src/main/java/com/exemplo/Controller/ViewController.java
@@ -1,23 +1,33 @@
 package com.exemplo.Controller;
 
-import com.exemplo.Util.Alerts;
+
+import java.util.Locale;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Labeled;
+import javafx.scene.control.TextField;
 
 public class ViewController {
     
     @FXML
-    private Button myButton;
+    private TextField soma1;
+    
+    @FXML
+    private TextField soma2;
+    
+    @FXML 
+    private Button btnSomar;
 
     @FXML
-    public void initialize() {
-        Alerts.showAlert(
-        "Aviso importante",           // title
-        "Verifique suas configurações", // header
-        "Certifique-se de que todos os campos foram preenchidos corretamente.", // content
-        Alert.AlertType.INFORMATION  // tipo de alerta
-    );
+    private Labeled labelResultado;
+
+    @FXML
+    public void onBtnSomarClick() {
+        Locale.setDefault(Locale.US);
+        double num1 = Double.parseDouble(soma1.getText());
+        double num2 = Double.parseDouble(soma2.getText());
+        double resultado = num1 + num2;
+        labelResultado.setText(String.format("%.2f", resultado));
     }
 }
